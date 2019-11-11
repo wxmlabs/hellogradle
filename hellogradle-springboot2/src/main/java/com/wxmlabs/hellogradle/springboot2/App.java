4a935181b2b28e9992aec93aab577fda5994bf54
Simon <simon@wxmlabs.com>
2019-11-12 02:31:59 +0800

    build executable war file

--- hellogradle-springboot2/src/main/java/com/wxmlabs/hellogradle/springboot2/App.java
+++ hellogradle-springboot2/src/main/java/com/wxmlabs/hellogradle/springboot2/App.java
@@ -2,10 +2,18 @@ package com.wxmlabs.hellogradle.springboot2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        builder.sources(App.class);
+        return super.configure(builder);
     }
 }
